b5abae85626ded5263c60f5bf8a6199b507f8edb
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-25 12:33:00 +0200

    Add loadEnviroments and start server

---
+++ src/server/index.ts
@@ -0,0 +1,22 @@
+import "../loadEnviroments";
+import morgan from "morgan";
+import express from "express";
+import cors from "cors";
+
+const allowedOrigins = process.env.ALLOWED_ORIGIN;
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors(options));
+
+app.disable("x-powered-by");
+
+app.use(morgan("dev"));
+
+export default app;
