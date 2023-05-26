c5df7a75450d50ee29585a3dea59eb477122b239
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-26 11:46:58 +0200

    Add login and user paths to paths utils

--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -3,13 +3,11 @@ import { type UserCredentials, type UserDataStructure } from "../server/types";
 export const user: UserCredentials = {
   username: "Judit",
   password: "Judit",
-  name: "Judit",
 };
 
 export const userData: UserDataStructure = {
   username: "Judit",
   password: "Judit",
-  name: "Judit",
   _id: "JuditVives",
 };
 
--- src/server/utils/paths/paths.ts
+++ src/server/utils/paths/paths.ts
@@ -2,6 +2,8 @@ import { type PathsStructure } from "../types";
 
 const paths: PathsStructure = {
   pingController: "/",
+  user: "/user",
+  login: "/login",
 };
 
 export default paths;
