32ebe6d6c428aeda8254c2da40874a4af64877cf
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-03 21:23:52 +0200

    Refactor getSynths function finding by userId

--- src/server/controllers/synths/synthsControllers.ts
+++ src/server/controllers/synths/synthsControllers.ts
@@ -1,10 +1,16 @@
-import { type NextFunction, type Response, type Request } from "express";
+import { type NextFunction, type Response } from "express";
 import Synth from "../../../database/models/Synths.js";
+import { type CustomRequest } from "../../types.js";
 import { responseStatusCode } from "../../utils/responseData/responseData.js";
 
-const getSynths = async (req: Request, res: Response, next: NextFunction) => {
+const getSynths = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userId } = req;
   try {
-    const synths = await Synth.find().limit(10).exec();
+    const synths = await Synth.find({ user: userId }).limit(10).exec();
 
     res.status(responseStatusCode.ok).json(synths);
   } catch (error: unknown) {
